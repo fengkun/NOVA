c9ab078e163a2ac434f0dd822d2073ae99c31eca
Udo Steinberg <udo@hypervisor.org>
2019-04-12 13:50:58 +0200

    ARM: Added Timeout classes.

---
+++ inc/aarch64/timeout.hpp
@@ -0,0 +1,47 @@
+/*
+ * Timeout
+ *
+ * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "compiler.hpp"
+#include "types.hpp"
+
+class Timeout
+{
+    private:
+        uint64              time            { 0 };
+        Timeout *           prev            { nullptr };
+        Timeout *           next            { nullptr };
+
+        static Timeout *    list            CPULOCAL;
+
+        virtual void trigger() = 0;
+
+    public:
+        // Enforce a constructor for CPU-local timeouts
+        ALWAYS_INLINE
+        inline Timeout() {}
+
+        ALWAYS_INLINE
+        inline bool active() const { return prev || list == this; }
+
+        void enqueue (uint64);
+        uint64 dequeue();
+
+        static void check();
+};
---
+++ src/aarch64/timeout.cpp
@@ -0,0 +1,78 @@
+/*
+ * Timeout
+ *
+ * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "timeout.hpp"
+#include "timer.hpp"
+
+Timeout *Timeout::list;
+
+void Timeout::enqueue (uint64 t)
+{
+    time = t;
+
+    Timeout *p = nullptr;
+
+    for (Timeout *n = list; n; p = n, n = n->next)
+        if (n->time >= time)
+            break;
+
+    prev = p;
+
+    if (!p) {
+        next = list;
+        list = this;
+        Timer::set_dln (time);
+    } else {
+        next = p->next;
+        p->next = this;
+    }
+
+    if (next)
+        next->prev = this;
+}
+
+uint64 Timeout::dequeue()
+{
+    if (active()) {
+
+        if (next)
+            next->prev = prev;
+
+        if (prev)
+            prev->next = next;
+
+        else if ((list = next))
+            Timer::set_dln (list->time);
+
+        else
+            Timer::set_dln (~0ULL);
+    }
+
+    prev = next = nullptr;
+
+    return time;
+}
+
+void Timeout::check()
+{
+    while (list && list->time <= Timer::time()) {
+        Timeout *t = list;
+        t->dequeue();
+        t->trigger();
+    }
+}
