77e3213c9db2511e71f77092dbba788c87b014eb
Udo Steinberg <udo@hypervisor.org>
2019-07-18 11:40:58 +0200

    ARM: Added Paging class.

--- inc/aarch64/paging.hpp
+++ inc/aarch64/paging.hpp
@@ -17,9 +17,30 @@
 
 #pragma once
 
+#ifdef __ASSEMBLER__
+
 #define PTE_P           (1 << 0)        // Present
 #define PTE_nL          (1 << 1)        // Not Block
 #define PTE_MT(X)       (X << 2)        // Memory Type
 #define PTE_SH          (3 << 8)        // Inner Shareable
 #define PTE_A           (1 << 10)       // Accessed
 #define PTE_nG          (1 << 11)       // Not Global
+
+#else
+
+class Paging
+{
+    public:
+        enum Permissions
+        {
+            R = 1UL << 0,       // Read
+            W = 1UL << 1,       // Write
+            X = 1UL << 2,       // Execute
+         // ? = 1UL << 3,       // API Accessible
+         // ? = 1UL << 4,       // API Accessible
+            U = 1UL << 12,      // User
+            G = 1UL << 13,      // Global
+        };
+};
+
+#endif
