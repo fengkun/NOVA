18bb1ab69bdf04281eaa4d0a45a0b9026919bad6
Udo Steinberg <udo@hypervisor.org>
2019-08-03 10:50:29 +0200

    ARM: Added GICR (Redistributor).

--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -19,6 +19,7 @@
 #include "cpu.hpp"
 #include "extern.hpp"
 #include "gicd.hpp"
+#include "gicr.hpp"
 #include "stdio.hpp"
 
 unsigned Cpu::id, Cpu::hazard, Cpu::boot_cpu, Cpu::online;
@@ -239,6 +240,7 @@ void Cpu::init (unsigned cpu, unsigned e)
                  TCR_A64_RES0;
 
     Gicd::init();
+    Gicr::init();
 
     boot_lock.unlock();
 }
